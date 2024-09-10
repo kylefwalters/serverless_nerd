2520091fef90a447639ec8d92af6220262da09d2
kylefwalters <41218512+kylefwalters@users.noreply.github.com>
2024-09-10 12:48:29 -0400

    Make function recursive

--- Week 1/IsPalindrome.js
+++ Week 1/IsPalindrome.js
@@ -1,6 +1,7 @@
 isPalindrome(process.argv[2], process.argv[3]);
 
 function isPalindrome(arg, enableDebug) {
+    // Check that argument is given
     if(!arg) {
         console.log("Invalid Argument");
         return;
@@ -9,22 +10,28 @@ function isPalindrome(arg, enableDebug) {
     // Strip non-alphabetic characters & unify casing
     arg = arg.replace(/([$&+,:;=?@#|'"<>.^*(){}%~_\[\]!-])|(\s)/g, "").toLowerCase();
 
-    // Iterate through string
-    let left = 0;
-    let right = arg.length - 1;
-    while(left < right) {
-        if(enableDebug == 'true') {
-            console.log("left = " + arg[left] + ", right = " + arg[right]);
-        }
+    // Iterate through string recursively
+    if(recursivePalidrome(arg, 0, enableDebug)) {
+        console.log("Is a palindrome");
+    } else {
+    console.log("Not a palindrome");
+    }
+}
 
-        if(arg[left] != arg[right]) {
-            console.log("Not a palindrome");
-            return false;
-        }
-        left++;
-        right--;
+function recursivePalidrome(arg, index, enableDebug) {
+    if(index >= (arg.length - 1) / 2) {
+        return true;
     }
 
-    console.log("Is a palindrome");
-    return true;
+    let left = index;
+    let right = arg.length - 1 - index;
+    if(enableDebug == 'true') {
+        console.log("left = " + arg[left] + ", right = " + arg[right]);
+    }
+
+    if(arg[left] != arg[right]) {
+        return false;
+    }
+
+    return recursivePalidrome(arg, index + 1, enableDebug);
 }
