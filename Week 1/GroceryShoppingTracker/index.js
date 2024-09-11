57bfdb90c1d8b46424d49462211e69d893072455
kylefwalters <41218512+kylefwalters@users.noreply.github.com>
2024-09-11 14:30:33 -0400

    Implement remove and modify functionality

--- Week 1/GroceryShoppingTracker/index.js
+++ Week 1/GroceryShoppingTracker/index.js
@@ -22,11 +22,11 @@ rl.on('line', (line) => {
 
 rl.once('close', () => {
     // end of input
-    console.log(`Goodbye, ${userName}`);
+    console.log(`\nGoodbye, ${userName}`);
 });
 
 function askToModifyList() {
-    rl.question("What would you like to do? (add/remove/modify)", (response) => {
+    rl.question("What would you like to do? (add/remove/bought/list)", (response) => {
         response = response.toLowerCase();
         switch(response) {
             case "add":
@@ -35,8 +35,12 @@ function askToModifyList() {
             case "remove":
                 removeItem();
                 break;
-            case "modify":
+            case "bought":
                 modifyItem();
+                break;
+            case "list":
+                showGroceryList();
+                askToModifyList();
                 break;
             default:
                 console.log("Invalid input, try again");
@@ -49,40 +53,54 @@ function askToModifyList() {
 function askForItemName() {
     rl.question("(1/3) What is the name of the item? ", (name) => {
         const groceryItem = new GroceryItem(name);
+
         askForItemQuantity(groceryItem);
     });
 }
 
 function askForItemQuantity(groceryItem) {
-    rl.question("(2/3) How many of the item do you want? ", (price) => {
-        groceryItem.price = price;
+    rl.question("(2/3) How many of the item do you want? ", (quantity) => {
+        groceryItem.quantity = quantity;
+
         askForItemPrice(groceryItem);
     });
 }
 
 function askForItemPrice(groceryItem) {
-    rl.question("(3/3) What is the price of the item? $", (quantity) => {
-        groceryItem.quantity = quantity;
+    rl.question("(3/3) What is the price of the item? $", (price) => {
+        groceryItem.price = price;
         groceryList.push(groceryItem);
-
         console.log("Great, item added");
+
         showGroceryList();
         askToModifyList();
     });
 }
 
 function removeItem() {
-    
+    showGroceryList();
+    rl.question("Type the index of the item you'd like to remove: ", (index) => {
+        groceryList.splice(index - 1, 1);
+        console.log("Item removed");
+        
+        askToModifyList();
+    });
 }
 
 function modifyItem() {
-
+    showGroceryList();
+    rl.question("Type the index of the item you have bought: ", (index) => {
+        groceryList[index - 1].bought = true;
+        console.log("Item marked as bought");
+        
+        askToModifyList();
+    });
 }
 
 function showGroceryList() {
-    console.log("______________________________________________");
+    console.log("______________________________________________________________");
     for(i = 0; i < groceryList.length; i++) {
-        console.log(i + ". " + groceryList[i].toString());
+        console.log((i + 1) + ". " + groceryList[i].toString());
     }
-    console.log("______________________________________________");
+    console.log("______________________________________________________________");
 }
